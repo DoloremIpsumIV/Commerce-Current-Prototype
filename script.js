b920143cdd7a8c3d114fa3a6322e8a8eb976ed36
Rasmus Jildholt <Rjildholt@gmail.com>
2024-03-17 22:42:14 +0100

    hmmmmm

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let prodNumber = 0;
 let storage = [];
 let popup;
-let
+let popupContent;
+let p;
 function init() {
+    p = document.querySelector("#hell");
     let purchase = document.querySelector("#PurchaseButton");
     let ProdInfo = document.querySelector("#ProdInfo");
     let Review = document.querySelector("#Recensioner");
     popup = document.querySelector("#Popup");
     let close = document.querySelector("#Close")
-
+    popupContent = document.querySelector("#popupContent")
 
     purchase.addEventListener("click", productTab);
     ProdInfo.addEventListener("click", productTab);
@@ -24,12 +26,18 @@ window.addEventListener("load", init);
 
 function productTab() {
     popup.classList.remove("Hide");
+    popupContent.innerHTML = "";
+    p.innerHTML = "";
     switch (this.id) {
         case "PurchaseButton":
+            p.innerHTML = "Lades till i varukorg";
+            popupContent.innerHTML = "";
             break;
         case "ProdInfo":
+            p.innerHTML = "Produkt info:";
             break;
         case "Recensioner":
+            p.innerHTML = "Recensioner:";
             break;
     }
 }
