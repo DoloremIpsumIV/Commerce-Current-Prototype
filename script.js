dba71e20598a1f556f431195d39d8de3bfb4a4fc
Rasmus Jildholt <Rjildholt@gmail.com>
2024-03-17 23:13:39 +0100

    alrightyalmost progress

--- script.js
+++ script.js
@@ -27,21 +27,24 @@ window.addEventListener("load", init);
 function productTab() {
     popup.classList.remove("Hide");
     popupContent.innerHTML = "";
+    popup.style.paddingBottom = "400px"
     p.innerHTML = "";
     switch (this.id) {
         case "PurchaseButton":
             p.innerHTML = "Lades till i varukorg";
+            popupContent.innerHTML = "<div id='glog'><a href='products.html'<div id='PurchaseButton'><button type='button'><p>Fortsätt handla</p></button></a> <a href='cart.html'<div id='PurchaseButton'><button type='button'><p>Till varukorgen</p></button></a></div>"
             break;
         case "ProdInfo":
             p.innerHTML = "Produkt info:";
+            popup.style.paddingBottom = "200px"
             popupContent.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took"
             break;
         case "Recensioner":
+            popup.style.paddingBottom = "50px"
             p.innerHTML = "Recensioner:";
             popupContent.innerHTML = " <img><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took</p><div class='rating'><p>2,1</p><img src='images/stjärna.png'></div>"
-            popupContent.innerHTML += " <img src='images/datorntorn.jpg' alt='bild på dator torn'><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took</p><div class='rating'><p>4,3</p><img src='images/stjärna.png'></div>"
+            popupContent.innerHTML += " <img src='images/bok.jpg' alt='bild på dator torn'><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took</p><div class='rating'><p>4,3</p><img src='images/stjärna.png'></div>"
             popupContent.innerHTML += " <img><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took</p><div class='rating'><p>5</p><img src='images/stjärna.png'></div>"
-            popupContent.innerHTML += " <img><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took</p><div class='rating'><p>3</p><img src='images/stjärna.png'></div>"
             break;
     }
 }
